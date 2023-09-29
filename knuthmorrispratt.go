e216be08413b09e8cddac240da7a57ac3160d797
HeigoTasuja <97948830+HeigoTasuja@users.noreply.github.com>
2023-09-29 11:27:16 +0000

    kmp GO

---
+++ knuthmorrispratt.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Example
+    fmt.Println(knuthMorrisPratt("abcde", "cd")) // Prints true
+}
+
+func knuthMorrisPratt(str, substr string) bool {
+    pattern := buildPattern(substr)
+    return matches(str, substr, pattern)
+}
+
+func buildPattern(substr string) []int {
+    pattern := make([]int, len(substr))
+    for i := range pattern {
+        pattern[i] = -1
+    }
+    j, i := 0, 0
+    for i < len(substr) {
+        if substr[i] == substr[j] {
+            pattern[i] = j
+            i++
+            j++
+        } else if j > 0 {
+            j = pattern[j-1]
