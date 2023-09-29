71344150ee3847cb92ef0a79fdb068b8fdea8d27
HeigoTasuja <97948830+HeigoTasuja@users.noreply.github.com>
2023-09-29 11:26:19 +0000

    kmp TS

---
+++ knuthmorrispratt.ts
@@ -0,0 +1,35 @@
+export function knuthMorrisPratt(string: string, substring: string): boolean {
+    const pattern = buildPattern(substring);
+    return matches(string, substring, pattern);
+}
+
+function buildPattern(substring: string): number[] {
+    const pattern: number[] = Array(substring.length).fill(-1);
+    let j = 0, i = 0;
+    while (i < substring.length) {
+        if (substring[i] === substring[j]) {
+            pattern[i] = j;
+            i++; j++;
+        } else if (j > 0) {
+            j = pattern[j - 1] + 1;
+        } else {
+            i++;
+        }
+    }
+    return pattern;
+}
+
+function matches(string: string, substring: string, pattern: number[]): boolean {
+    let i = 0, j = 0;
+    while (i + substring.length - j <= string.length) {
+        if (string[i] === substring[j]) {
+            if (j === substring.length - 1) return true;
+            i++; j++;
+        } else if (j > 0) {
+            j = pattern[j - 1] + 1;
+        } else {
+            i++;
+        }
+    }
+    return false;
+}
